3fc08b8e7230181e335085cd13c5d5413a976f5a
Kriti-Srivastava <kriti.g.srivastava@gmail.com>
2023-01-07 19:03:42 -0500

    initial version.

---
+++ src/main/java/org/ctemc/BlockchainBasedStudentVerification/BlockchainBasedStudentVerificationApplication.java
@@ -0,0 +1,14 @@
+package org.ctemc.BlockchainBasedStudentVerification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockchainBasedStudentVerificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlockchainBasedStudentVerificationApplication.class, args);
+		System.out.println("hello blockchain");
+	}
+
+}
---
+++ src/test/java/org/ctemc/BlockchainBasedStudentVerification/BlockchainBasedStudentVerificationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ctemc.BlockchainBasedStudentVerification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockchainBasedStudentVerificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
